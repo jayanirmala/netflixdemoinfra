4bbf56c0f33cfebc55c6bef613696e91df14042d
jayanirmala <jayanirmalar1@gmail.com>
2025-04-24 15:19:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-07a6f770277670015"
-instance_type = "t2.medium"
+count = 5
+ami = "ami-0b86aaed8ef90e45f"
+instance_type = "t2.micro"
 key_name = "key01"
 vpc_security_group_ids = ["sg-023b01ee03394b2d8"]
 tags = {
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["ansible", "dev-1", "dev-2", "test-1", "test-2"]
 }
